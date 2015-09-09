3e776b085896ef6c373ffa14bf62f593725348e1
James McKinney <james@slashpoundbang.com>
2015-09-09 15:14:21 -0400

    improve Response docs

--- lib/whos_got_dirt/response.rb
+++ lib/whos_got_dirt/response.rb
@@ -15,18 +15,37 @@ module WhosGotDirt
   #
   # @example Use the class to parse a response.
   #   MyAPIResponse.new(response).to_a
-  #   #=> [{
-  #     :request_url=>"https://api.example.com/endpoint?name=John+Smith",
-  #     :name=>"John Smith"
-  #   }, {
-  #     :request_url=>"https://api.example.com/endpoint?name=John+Smith",
-  #     :name=>"John Aaron Smith"
-  #   }]
+  #   # [{
+  #   #   :request_url=>"https://api.example.com/endpoint?name=John+Smith",
+  #   #   :name=>"John Smith"
+  #   # }, {
+  #   #   :request_url=>"https://api.example.com/endpoint?name=John+Smith",
+  #   #   :name=>"John Aaron Smith"
+  #   # }]
   class Response < SimpleDelegator
     # @abstract Subclass and override {#to_a} to return the results in the
-    #   response, or an empty array if an error occurred
+    #   response body, or an empty array if an error occurred
     def to_a
       raise NotImplementedError
     end
+
+    # @!method initialize(response)
+    #   @param [Faraday::Response] response a response
+
+    # @!method body
+    #   Returns the response body.
+    #   @return [String] the response body
+
+    # @!method env
+    #   Returns the response's internals.
+    #   @return [#url,#request_headers] the response's internals
+
+    # @!method headers
+    #   Returns the response headers.
+    #   @return [#[]] the response headers
+
+    # @!method status
+    #   Returns the HTTP status code.
+    #   @return [Fixnum] the HTTP status code
   end
 end
