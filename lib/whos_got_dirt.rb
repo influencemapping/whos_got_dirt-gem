6e8dfcc30ebfcb36bbc04e7fd257108fb3e6097b
James McKinney <james@slashpoundbang.com>
2015-09-10 18:43:46 -0400

    add json-schema gem, add WhosGotDirt.schemas method

--- lib/whos_got_dirt.rb
+++ lib/whos_got_dirt.rb
@@ -2,6 +2,7 @@ require 'cgi'
 
 require 'active_support/core_ext/hash/indifferent_access'
 require 'json-pointer'
+require 'json-schema'
 
 require 'whos_got_dirt/renderer'
 
@@ -10,3 +11,15 @@ require 'whos_got_dirt/requests/person/open_corporates'
 
 require 'whos_got_dirt/response'
 require 'whos_got_dirt/responses/person/open_corporates'
+
+module WhosGotDirt
+  class << self
+    def schemas
+      @schemas ||= {}.tap do |hash|
+        Dir[File.expand_path(File.join('..', '..', 'schemas', '*'), __FILE__)].each do |file|
+          hash[File.basename(file, '.json')] = JSON.load(File.read(file))
+        end
+      end
+    end
+  end
+end
---
+++ spec/whos_got_dirt_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+RSpec.describe WhosGotDirt do
+  describe '.schemas' do
+    it 'should return the schemas' do
+      expect(WhosGotDirt.schemas['person'].class).to eq(Hash)
+    end
+  end
+end
