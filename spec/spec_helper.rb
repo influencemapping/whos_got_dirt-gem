1450d2677e31b263f21415467a79878afcc51d3a
James McKinney <james@slashpoundbang.com>
2015-09-11 00:28:30 -0400

    use vcr for faster tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,13 @@ SimpleCov.start do
   add_filter 'spec'
 end
 
-require 'faraday'
+require 'vcr'
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :faraday
+  c.configure_rspec_metadata!
+end
 
 require 'rspec'
+require 'faraday'
 require File.dirname(__FILE__) + '/../lib/whos_got_dirt'
