0c16787de8b48b2ee6c5f22f385c8d442bfc5b0e
James McKinney <james@slashpoundbang.com>
2015-09-11 00:28:51 -0400

    add OpenCorporates person response handler

--- lib/whos_got_dirt/responses/person/open_corporates.rb
+++ lib/whos_got_dirt/responses/person/open_corporates.rb
@@ -1,28 +1,84 @@
 module WhosGotDirt
   module Responses
     module Person
+      # Converts corporate officers from the OpenCorporates API to Popolo format.
+      #
+      # @see http://api.opencorporates.com/documentation/REST-API-introduction
       class OpenCorporates < Response
-        MAP = {
-          # @todo
+        @template = {
+          'name' => '/name',
+          'birth_date' => '/date_of_birth',
+          'updated_at' => '/retrieved_at',
+          'identifiers' => [{
+            'identifier' => '/id',
+            'scheme' => 'OpenCorporates',
+          }, {
+            'identifier' => '/uid',
+            'scheme' => 'Company Register',
+          }],
+          'contact_details' => [{
+            'type' => 'address',
+            'value' => '/address',
+          }],
+          'links' => [{
+            'url' => '/opencorporates_url',
+            'note' => 'OpenCorporates URL',
+          }],
+          'memberships' => [{
+            'role' => '/position',
+            'start_date' => '/start_date',
+            'end_date' => '/end_date',
+            'organization' => {
+              'name' => '/company/name',
+              'identifiers' => [{
+                'identifier' => '/company/company_number',
+                'scheme' => 'Company Register',
+              }],
+              'links' => [{
+                'url' => '/company/opencorporates_url',
+                'note' => 'OpenCorporates URL',
+              }],
+              # API-specific.
+              'jurisdiction_code' => '/company/jurisdiction_code',
+            },
+            # API-specific.
+            'inactive' => '/inactive',
+          }],
+          # API-specific.
+          'current_status' => '/current_status',
+          'jurisdiction_code' => '/jurisdiction_code',
+          'nationality' => '/nationality',
+          'occupation' => '/occupation',
         }
 
-        # Parses a response into results.
+        # Parses the response body.
+        #
+        # @return [Array<Hash>] the parsed response body
+        def parse_body
+          JSON.load(body)['results']
+        end
+
+        # Transforms the parsed response body into results.
         #
         # @return [Array<Hash>] the results
         def to_a
-          # @see http://api.opencorporates.com/documentation/REST-API-introduction
-          if status == 200
-            # @todo respect `null` in MQL
-            JSON.load(body)['results'].map do |result|
-              hash = {}
-              result.each do |key,value|
-                hash[MAP.fetch(key, key)] = value
-              end
-              hash
-            end
-          else
-            []
+          parsed_body['officers'].map do |data|
+            Result.new(renderer.result(data['officer']), self, 'person').finalize!
           end
+        end
+
+        # Returns the total number of matching results.
+        #
+        # @return [Fixnum] the total number of matching results
+        def count
+          parsed_body['total_count']
+        end
+
+        # Returns the current page number.
+        #
+        # @return [Fixnum] the current page number
+        def page
+          parsed_body['page']
         end
       end
     end
--- spec/responses/person/open_corporates_spec.rb
+++ spec/responses/person/open_corporates_spec.rb
@@ -1,7 +1,33 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 module WhosGotDirt::Responses::Person
-  RSpec.describe OpenCorporates do
-    # @todo validate against Popolo schema
+  RSpec.describe OpenCorporates, :vcr => {cassette_name: 'opencorporates_person'} do
+    let :response do
+      Faraday.get('https://api.opencorporates.com/officers/search')
+    end
+
+    let :instance do
+      OpenCorporates.new(response)
+    end
+
+    describe '#to_a' do
+      it 'should return the results' do
+        StackProf.run(mode: :cpu, out: '/tmp/whos_got_dirt.dump') do
+          expect{instance}.to_not raise_error
+        end
+      end
+    end
+
+    describe '#count' do
+      it 'should return the number of results' do
+        expect(instance.count).to be > 100_000_000
+      end
+    end
+
+    describe '#page' do
+      it 'should return the current page number' do
+        expect(instance.page).to eq(1)
+      end
+    end
   end
 end
