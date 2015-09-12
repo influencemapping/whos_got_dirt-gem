4bcabd8636a8ac43233b18077a37a66d903d2d3b
James McKinney <james@slashpoundbang.com>
2015-09-12 15:55:17 -0400

    add require for new request handler

--- lib/whos_got_dirt.rb
+++ lib/whos_got_dirt.rb
@@ -11,6 +11,7 @@ require 'whos_got_dirt/validator'
 
 require 'whos_got_dirt/request'
 require 'whos_got_dirt/requests/person/open_corporates'
+require 'whos_got_dirt/requests/organization/open_corporates'
 
 require 'whos_got_dirt/response'
 require 'whos_got_dirt/responses/person/open_corporates'
