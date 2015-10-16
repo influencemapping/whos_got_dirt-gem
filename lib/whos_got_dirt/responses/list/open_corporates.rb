4e0a77c4ae94b2c8ed634f0cc1e70fad90124779
James McKinney <james@slashpoundbang.com>
2015-10-16 17:03:20 -0400

    add OC corporate_groupings/search response handler

---
+++ lib/whos_got_dirt/responses/list/open_corporates.rb
@@ -0,0 +1,34 @@
+module WhosGotDirt
+  module Responses
+    module List
+      # Converts corporate groupings from the OpenCorporates API to Popolo format.
+      #
+      # @see http://api.opencorporates.com/documentation/REST-API-introduction
+      class OpenCorporates < Helpers::OpenCorporatesHelper
+        @template = {
+          '@type' => 'ListItem',
+          'name' => '/name',
+          'created_at' => '/created_at',
+          'updated_at' => '/updated_at',
+          'identifiers' => [{
+            'identifier' => '/wikipedia_id',
+            'scheme' => 'Wikipedia',
+          }],
+          'links' => [{
+            'url' => '/opencorporates_url',
+            'note' => 'OpenCorporates URL',
+          }],
+        }
+
+        # Transforms the parsed response body into results.
+        #
+        # @return [Array<Hash>] the results
+        def to_a
+          parsed_body['corporate_groupings'].map do |data|
+            Result.new('Organization', renderer.result(data['corporate_grouping']), self).finalize!
+          end
+        end
+      end
+    end
+  end
+end
